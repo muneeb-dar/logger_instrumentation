08908d1ca658ef605dadaeb322c9362e99ad7158
muneeb-dar <93276208+muneeb-dar@users.noreply.github.com>
2022-04-11 22:15:04 +0530

    Create DemoServlet3Instrumentation.java

---
+++ DemoServlet3Instrumentation.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.example.javaagent.instrumentation;
+
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
+import io.opentelemetry.javaagent.instrumentation.api.Java8BytecodeBridge;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+import net.bytebuddy.matcher.ElementMatchers;
+
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.implementsInterface;
+import static net.bytebuddy.matcher.ElementMatchers.isNamed;
+import static net.bytebuddy.matcher.ElementMatchers.isPublic;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.namedOneOf;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+
+public class DemoServlet3Instrumentation implements TypeInstrumentation {
+  @Override
+  public ElementMatcher<ClassLoader> classLoaderOptimization() {
+    return hasClassesNamed("org.slf4j.Logger");
+  }
+  @Override
+  public ElementMatcher<TypeDescription> typeMatcher() {
+    return implementsInterface(named("org.slf4j.Logger"));
+  }
+
+  @Override
+  public void transform(TypeTransformer typeTransformer) {
+    typeTransformer.applyAdviceToMethod(
+        named("error")
+            .and(
+                takesArgument(
+                    0, named("java.lang.String")))
+            .and(
+                takesArgument(
+                    1, named("java.lang.Throwable")))
+            .and(isPublic()),
+        this.getClass().getName() + "$DemoServlet3Advice");
+  }
+
+  @SuppressWarnings("unused")
+  public static class DemoServlet3Advice {
+
+    @Advice.OnMethodEnter(suppress = Throwable.class)
+    public static void onEnter(@Advice.Argument(0) String  message,@Advice.Argument(1) Throwable  throwable){
+      Span.current().recordException(throwable);
+      System.out.println("Testing otel");
+      Java8BytecodeBridge.currentSpan().recordException(throwable);
+      Java8BytecodeBridge.currentSpan().setAttribute("TESTING","HI THERE");
+
+
+      }
+    }
+  }
