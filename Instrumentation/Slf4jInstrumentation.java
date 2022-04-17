4bddc434c81fddd0bc11e2f3e0f5ae3ec9bca5c4
muneeb-dar <muneeb.dar@getfareye.com>
2022-04-18 01:10:25 +0530

    recent changes

--- Instrumentation/Slf4jInstrumentation.java
+++ Instrumentation/Slf4jInstrumentation.java
@@ -5,24 +5,19 @@
 
 package com.example.javaagent.instrumentation;
 
-import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
 import io.opentelemetry.javaagent.instrumentation.api.Java8BytecodeBridge;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
-import net.bytebuddy.matcher.ElementMatchers;
 
 import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.implementsInterface;
-import static net.bytebuddy.matcher.ElementMatchers.isNamed;
 import static net.bytebuddy.matcher.ElementMatchers.isPublic;
 import static net.bytebuddy.matcher.ElementMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.namedOneOf;
-import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
 
-public class Slf4jInstrumentation implements TypeInstrumentation {
+public class DemoServlet3Instrumentation implements TypeInstrumentation {
   @Override
   public ElementMatcher<ClassLoader> classLoaderOptimization() {
     return hasClassesNamed(new StringBuilder("org.").append("slf4j.Logger").toString());
@@ -36,32 +31,31 @@ public class Slf4jInstrumentation implements TypeInstrumentation {
   public void transform(TypeTransformer typeTransformer) {
     typeTransformer.applyAdviceToMethod(
         named("error")
-            .and(
-                takesArgument(
-                    0, named("java.lang.String")))
-            .and(
-                takesArgument(
-                    1, named("java.lang.Throwable")))
             .and(isPublic()),
-        this.getClass().getName() + "$LoggerAdvice");
+        this.getClass().getName() + "$DemoServlet3Advice");
   }
 
   @SuppressWarnings("unused")
-  public static class LoggerAdvice {
+  public static class DemoServlet3Advice {
 
     @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static void onEnter(@Advice.Argument(0) String  message,@Advice.Argument(1) Throwable  throwable){
+    public static void onEnter(@Advice.AllArguments Object[] args){
       /*
       instead of span.current use Java8BytecodeBridge.currentSpan()
-       */
-      Java8BytecodeBridge.currentSpan().setAttribute("error",true);
-      Java8BytecodeBridge.currentSpan().recordException(throwable);
-      /*
-      Span.current().recordException(throwable);
-      Java8BytecodeBridge.currentSpan().setAttribute("TESTING","HI THERE");
-       */
 
+      Instead of this-----> Span.current().recordException(throwable);
+      Use This------> Java8BytecodeBridge.currentSpan().recordException(throwable)
 
+       */
+      for(Object arg:args)
+      {
+        if (arg instanceof Throwable) {
+          Throwable throwable = (Throwable) arg;
+          Java8BytecodeBridge.currentSpan().setAttribute("error",true);
+          Java8BytecodeBridge.currentSpan().recordException(throwable);
+
+        }
+      }
       }
     }
   }
